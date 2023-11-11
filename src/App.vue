8c1d99145d1389bfc77faab3aba0acf80a3a97a5
bernal9913 <carlosbernal@Carloss-MacBook-Air.local>
2023-11-11 11:54:31 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>PErfil de usuario</h1>
+    <user-card
+        user-bio="Desarrollador movil frustrado usando vue"
+        user-email="bernalgas@gmail.com"
+        user-name="Carlos ramirez"
+        user-image="https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png"
+    />
+
+  </div>
+</template>
+<script>
+import UserCard from "@/UserCard.vue";
+export default {
+  components:{
+    UserCard,
+  },
+};
+</script>
---
+++ src/UserCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="user-card">
+    <img :src="userImage" alt="avatar" class="user-avatar"/>
+    <h2 class="titulo">{{ userName }}</h2>
+    <p class="texto">{{ userEmail }}</p>
+    <p class="texto">{{ userBio }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    userName:{
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    userBio:{
+      type: String,
+      required: true,
+    },
+    userImage:{
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style>
+.user-card{
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin: 16px;
+  width: 300px;
+  background-color: #f8f8f8;
+  border-radius: 12px;
+  box-shadow: 0 0 5px rgba(0,0,0,0.2);
+  text-align: center;
+
+}
+.texto{
+  color: #2c3e50;
+}
+.titulo{
+  color: #181818;
+}
+.user-avatar{
+  max-width: 100%;
+  height: auto;
+  //border-radius: 89%;
+}
+</style>
